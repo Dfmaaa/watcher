978aba098c409fb231aeccf8838beb86f141ef9e
Sameer Achhab <sameerkw23@gmail.com>
2025-12-23 10:46:52 +0530

    I have a concrete idea of where I wanna go with this

--- watcher.c
+++ watcher.c
@@ -1,45 +1,4 @@
-#define _GNU_SOURCE
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <dirent.h>
-#include "str_ll.h"
-
-struct fnode{
-
-    char *location;
-    struct str_blk *text; 
-    struct stat st;
-
-};
-
-struct dnode{
-
-    char *location; 
-    struct stat st;
-    struct dnode *parent;
-    struct dnode_list *dlist; 
-    struct fnode_list *flist;
-
-};
-
-struct dnode_list{
-
-    struct dnode *val;
-    struct dnode_list *next;
-
-};
-struct fnode_list{
-
-    struct fnode *val;
-    struct fnode_list *next;
-
-};
+#include "watcher.h"
 
 void *mmalloc(size_t size){
 
@@ -322,7 +281,17 @@ void trv_tree_test(struct dnode *root){
 
 }
 
-int main(){
+struct dnode *get_tree(char *location){
+
+    struct dnode *loc = init_dnode(location);
+
+    construct_base_tree(loc, NULL);
+
+    return loc;
+
+}
+
+/*int main(){
 
     struct dnode *loc = init_dnode("/home/dfmaaa1/Samex");
     
@@ -334,4 +303,4 @@ int main(){
 
     return 0;
 
-}
+}*/
---
+++ watcher.h
@@ -0,0 +1,44 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include "str_ll.h"
+
+struct fnode{
+
+    char *location;
+    struct str_blk *text; 
+    struct stat st;
+
+};
+
+struct dnode{
+
+    char *location; 
+    struct stat st;
+    struct dnode *parent;
+    struct dnode_list *dlist; 
+    struct fnode_list *flist;
+
+};
+
+struct dnode_list{
+
+    struct dnode *val;
+    struct dnode_list *next;
+
+};
+struct fnode_list{
+
+    struct fnode *val;
+    struct fnode_list *next;
+
+};
+
+struct dnode *get_tree(char *);
